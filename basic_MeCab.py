8fe75af4e5f826bcfd1606f886ad8af23fb42deb
mf1611 <masashi.16@gmail.com>
2019-10-06 17:14:10 +0900

    minor change

--- basic_MeCab.py
+++ basic_MeCab.py
@@ -1,4 +1,5 @@
 import MeCab
+import neologdn  # 表記ゆれの是正をしてくれるlibrary
 
 """
 ・WindowsでのMeCabのインストール
@@ -23,16 +24,58 @@ https://www.pytry3g.com/entry/MeCab-NEologd-Windows
     - $ sudo cp /usr/lib/mecab/dic/mecab-ipadic-neologd/* /mnt/c/neologd/
 """
 
-sample = "8月3日に放送された「中居正広の金曜日のスマイルたちへ」\n　\
-            私は東京に住んでいました。\n \
-            私は横浜に住んでいました。"
+sample = "8月3日に放送された「中居正広の金曜日のスマイルたちへ」"
 
 # 形態素解析
-tagger = MeCab.Tagger(r"-Ochasen -d C:\neologd")
-result = tagger.parse(sample)
+tagger_chasen = MeCab.Tagger(r"-Ochasen -d C:\neologd")
+result = tagger_chasen.parse(sample)
 print(result)
 
 # 分かち書き
-tagger = MeCab.Tagger(r"-Owakati -d C:\neologd")
-result = tagger.parse(sample)
+tagger_wakati = MeCab.Tagger(r"-Owakati -d C:\neologd")
+result = tagger_wakati.parse(sample)
 print(result)
+
+
+# dataframeによる品詞情報の格納
+# 品詞(type)での絞り込み、不要な品詞の除去など
+# https://datumstudio.jp/blog/pythonによる日本語前処理備忘録
+import pandas as pd
+
+def parse_text(text: str):
+    parsed_text = tagger_chasen.parse(text).split('\n')  # 改行で区切る
+    parsed_results = pd.Series(parsed_text).str.split('\t').tolist()  # 半角区切りでリストにし、それをSeriesにする
+    df = pd.DataFrame.from_records(parsed_results)
+    columns = ['surface', 'spell', 'orig', 'type', 'katsuyoukei', 'katsuyoukata']
+    df.columns = columns
+    return df.query("surface != 'EOS'").query("surface != ''")
+
+
+print(parse_text('庭には二羽鶏がいる'))
+
+
+# 名詞を中心とした形態素のみを抽出する関数
+def extract_noun(text: str):
+    norm_text = neologdn.normalize(text)
+    parsed = parse_text(norm_text)
+    noun_df = parsed[
+        parsed.type.str.startswith('名詞-一般') | 
+        parsed['type'].str.startswith('名詞-固有名詞') |
+        parsed.type.str.startswith('名詞-サ変接続') |
+        parsed.type.str.startswith('名詞-形容動詞語幹')
+    ]
+    return ' '.join(noun_df.orig.tolist())
+
+
+text = '私達はラーメンが大好きです。'
+print(parse_text(text))
+
+split_text = extract_noun(text)
+print(split_text)
+
+
+# Bag of Words
+# from sklearn.feature_extraction.text import CountVectorizer
+# # ワードカウント疎行列の抽出
+# cv = CountVectorizer()
+# cv.fit_transform()
